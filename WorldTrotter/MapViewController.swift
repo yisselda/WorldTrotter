8745f24dbf1822c13ceda5a50f9075d4564f00b5
Yisselda <yisselda@defmethod.io>
2019-05-04 18:11:20 -0400

    Add location button with shadow

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     
     var mapView: MKMapView!
+    var userLocButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView = MKMapView()
         view = mapView
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
@@ -37,6 +42,39 @@ class MapViewController: UIViewController {
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
+        
+//        Button to track user's location
+//        Space it at the bottom right in relation to the bottomLayerGuide
+        userLocButton = UIButton()
+        view.addSubview(userLocButton)
+        userLocButton.addTarget(self, action: #selector(showUserLocation(_:)), for: .touchUpInside)
+        userLocButton.translatesAutoresizingMaskIntoConstraints = false
+        userLocButton.setImage(UIImage(named: "marker"), for: .normal)
+        userLocButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        userLocButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        userLocButton.backgroundColor = .white
+        userLocButton.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -8).isActive = true
+        userLocButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 8).isActive = true
+        userLocButton.layer.cornerRadius = 25
+        userLocButton.layer.shadowColor = UIColor.gray.cgColor
+        userLocButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        userLocButton.layer.shadowOpacity = 1.0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("stuff")
+    }
+    
+    @IBAction func showUserLocation(_ sender: UIButton) {
+        print("Shit's happeneing")
+    }
+    
+    func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
+        debugPrint("startLocating")
+    }
+    func mapViewDidStopLocatingUser(_ mapView: MKMapView) {
+        debugPrint("stopLocating")
     }
     
     @objc
